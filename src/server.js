2e7fbb168956f9eca4476e56130ec9430958f59a
JamesHarrisonZa <james.harrison.za@gmail.com>
2019-06-01 11:15:05 +0200

    /login & /callback routes

--- src/server.js
+++ src/server.js
@@ -1,51 +1,54 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const request = require('request');
 const SpotifyWebApi = require('spotify-web-api-node');
 
-(async ()=> {
-
-	const app = express();
-	app.use(bodyParser.json()); //Needed for Post requests
-    app.use(bodyParser.urlencoded({ extended: true })); //Needed for Post requests
-    app.use((request, response, next) => { //Needed for cross origin
-        response.header("Access-Control-Allow-Origin", "*"); 
-        response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-    });
-
-    const spotify = new SpotifyWebApi({
-        clientId: process.env.SPOTIFY_CLIENT_ID,
-        clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-        redirectUri: process.env.SPOTIFY_REDIRECT_URI,
-    });
-
-	app.get('/', (req, res) => {
-		res.send('welcome to my API!');
-	});
-
-    app.post('/', (request, response) => {
-        const code = request.query.code;
-        if (!code) {
-            return response.status(403).send('Require code query parameter');
-        }
-        return spotify.authorizationCodeGrant(code)
-            .then((data) => {
-                response.json(data.body);
-                console.debug('success', request, response);
-                return response;
-            })
-            .catch((error) => {
-                response.status(500).send(error);
-                console.error('error', error, request, response);
-                return response;
-            });
-    });
-
-	const port = process.env.PORT || 42420;
-	app.listen(port, () => {
-		console.log(`Listening on http://localhost:${port}`);
-    });
-
-})().catch((error) => {
-	console.log(error);
+const app = express();
+    
+require('dotenv').config(); //ToDo: This is only needed for local. 
+const spotify = new SpotifyWebApi({
+    clientId: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    redirectUri: process.env.SPOTIFY_REDIRECT_URI,
 });
+
+app.get('/login', function (req, res) {
+
+    const scopes = ['user-library-read', 'user-modify-playback-state', 'user-read-private', 'user-read-email'];
+    const state = {};
+    const authorizeURL = spotify.createAuthorizeURL(scopes, state);
+    res.redirect(authorizeURL);
+});
+
+app.get('/callback', function (req, res) {
+    const code = req.query.code || null
+    console.log(`Got Access token: ${code}`);
+    //ToDo: Change to use spotify-web-api-node
+    const authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        form: {
+            code: code,
+            redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
+            grant_type: 'authorization_code'
+        },
+        headers: {
+            'Authorization': 'Basic ' + (new Buffer(
+                process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
+            ).toString('base64'))
+        },
+        json: true
+    }
+    request.post(authOptions, function (error, response, body) {
+        const access_token = body.access_token;
+        const uri = process.env.FRONTEND_URI || 'http://localhost:3000';
+        res.redirect(uri + '?access_token=' + access_token);
+    });
+});
+
+console.log(`SPOTIFY_CLIENT_ID: ${process.env.SPOTIFY_CLIENT_ID}`);
+console.log(`SPOTIFY_CLIENT_SECRET: ${process.env.SPOTIFY_CLIENT_SECRET}`);
+console.log(`SPOTIFY_REDIRECT_URI: ${process.env.SPOTIFY_REDIRECT_URI}`);
+
+const port = process.env.PORT || 42420;
+console.log(`Listening on port http://localhost:${port}`);
+console.log(`Go to http://localhost:${port}/login to initiate authentication flow.`);
+app.listen(port);
